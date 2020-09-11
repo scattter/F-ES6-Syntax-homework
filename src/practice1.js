a023993f2efab01dfccf663d4b007eb265279810
scattter <413365742@qq.com>
2020-09-11 09:14:47 +0800

    [zk] finsh test-2

--- src/practice1.js
+++ src/practice1.js
@@ -1,2 +1,12 @@
-const parseData
+const parseData = (input) => {
+    let output = [];
+    input.data.map(it => {
+        let item = {};
+        input.column.map(key => {
+            item[key.name] = it.shift()
+        })
+        output.push(item)
+    });
+    return output;
+}
 export { parseData };
--- src/practice2.js
+++ src/practice2.js
@@ -1,2 +1,23 @@
-const inject
+const inject = (lst1, lst2) => {
+    let index = []
+    let newLst = []
+    lst2.map(it => {
+        index.push(it.index)
+    })
+
+    const size = lst1.length + lst2.length
+    var acount = 0;
+    for (var id = 0; id < size; id++) {
+        if (index.length > 0 & id - acount === index[0]) {
+            index.shift()
+            newLst.push(lst2.find(con => {
+                return con.index === id - acount
+            }).content)
+            acount++;
+        } else {
+            newLst.push(lst1.shift())
+        }
+    }
+    return newLst
+}
 export { inject };
