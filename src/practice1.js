6f62bc484fc1e39efdf5f1b644d9b3828db2dfb5
kevindongzg <dongkevin94@gmail.com>
2020-09-22 20:36:18 +0800

    add TODO

--- src/practice1.js
+++ src/practice1.js
@@ -1,7 +1,10 @@
+// TODO 可以思考一下可否用reduce来做
 const parseData = ({ data, column }) => {
+    // TODO 使用map做数组与数组的转化，不用声明新数组
     let output = [];
     data.map(it => {
         let item = {};
+        // TODO 不是map使用场景
         column.map(key => {
             item[key.name] = it.shift()
         })
--- src/practice2.js
+++ src/practice2.js
@@ -1,3 +1,4 @@
+// TODO 逻辑纰漏，当sections内items顺序不是index顺序时会有问题
 const inject = (items, sections) => {
     sections.forEach((section, index) => {
         items.splice(section.index + index, 0, section.content)
